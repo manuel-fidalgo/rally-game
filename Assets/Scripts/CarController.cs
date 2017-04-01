271292e0d3a64d44c4694984437dca879341f656
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2017-04-01 20:42:54 +0200

    First commit, added car controller scrip file

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    Vector3 direction;
+    double speed;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starts the Car Controller");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Testing
+        gameObject.transform.Rotate(Vector3.up, 1);
+    }
+   
+}
