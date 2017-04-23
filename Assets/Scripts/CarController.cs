664cc1df75b7840dec7bf0c29bc527c10af1a938
Manuel <mfidaf01@gmail.com>
2017-04-23 17:14:13 +0200

    Added car picker scenene with color choocher, removed porsche and addded dodge

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,37 +1,115 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-        
-public class CarController : MonoBehaviour
-{
+
+public class CarController : MonoBehaviour {
+
+    private static readonly int LEFT = 0;
+    private static readonly int RIGHT = 1;
+
+
     public List<WheelCollider> frontWheels; // front axle
     public List<WheelCollider> rearWheels;  //rear axle
+
+    public List<GameObject> frontWheelsMesh;
+    public List<GameObject> rearWheelsMesh;
+
+    public Material ColorMaterial;
 
     public Transform massCentre;
     public Rigidbody bodyCar;
 
     public float maxMotorTorque;    // maximum torque to wheel
     public float maxSteeringAngle;  // maximum steer angle the wheel can have
+    public float BrakeTorquePower;
 
-    public void Start()
-    {
-        
+
+    private float currentMotorTorque;
+    private float currentBrakeTorque;
+    private float currentSteering;
+
+    public void Start() {
+
+        currentMotorTorque = 0;
+        currentBrakeTorque = 0;
+        currentSteering = 0;
+
     }
-    //Shoudl i use Update or FixedUpdate?
-    public void FixedUpdate()
-    {   
+
+    public void FixedUpdate() {
+
         //Updates the centerofmass
         bodyCar.centerOfMass = massCentre.localPosition;
+        manageInput();
 
-        //gets the torque steering values
-       float motor = maxMotorTorque * Input.GetAxis("Vertical");
-       float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-        
-       frontWheels[0].steerAngle = steering;
-       frontWheels[1].steerAngle = steering;
-            
-       rearWheels[0].motorTorque = motor;
-       rearWheels[1].motorTorque = motor;
-            
+        //Updates the torques and the steerings
+        frontWheels[LEFT].steerAngle = currentSteering;
+        frontWheels[RIGHT].steerAngle = currentSteering;
+
+
+        rearWheels[LEFT].motorTorque = currentMotorTorque;
+        rearWheels[RIGHT].motorTorque = currentMotorTorque;
+
+        rearWheels[LEFT].brakeTorque = currentBrakeTorque;
+        rearWheels[RIGHT].brakeTorque = currentBrakeTorque;
+
+        //wheelsAnimation();       
+
     }
+
+    public void manageInput() {
+
+        currentSteering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        currentMotorTorque = maxMotorTorque * Input.GetAxis("Vertical");
+
+        
+        if (Input.GetKey("space"))
+            toBrake();
+        else
+            currentBrakeTorque = 0.0f;
+       
+    }
+
+    public void toBrake() {
+
+        var rigidbody = gameObject.GetComponent<Rigidbody>();
+        currentBrakeTorque = rigidbody.mass * BrakeTorquePower;
+        currentMotorTorque = 0.0f;
+    }
+
+    public float carSpeedFromRMP(float rpm) {
+        float radius = rearWheels[0].radius; //in meters
+
+        return ((2 * Mathf.PI * rpm) / 60) * 3.6f; //* 3.6 transforms m/s -> kmh
+    }
+
+
+
+    public void wheelsAnimation() {
+
+        if (frontWheelsMesh != null && rearWheelsMesh != null) {
+
+            float rotation_angle = 0;
+            Transform FR, FL, RL, RR;
+            FL = frontWheelsMesh[LEFT].GetComponent<Transform>();
+            FR = frontWheelsMesh[RIGHT].GetComponent<Transform>();
+            RL = rearWheelsMesh[LEFT].GetComponent<Transform>();
+            RR = rearWheelsMesh[RIGHT].GetComponent<Transform>();
+
+
+            FL.transform.rotation = Quaternion.AngleAxis(currentSteering, Vector3.up);
+            FR.transform.rotation = Quaternion.AngleAxis(currentSteering, Vector3.up);
+
+            float rpm = (rearWheels[LEFT].rpm + rearWheels[RIGHT].rpm) / 2;
+            rotation_angle = (rpm * 360) / 60 * Time.fixedDeltaTime;
+
+            FL.transform.rotation = Quaternion.AngleAxis(rotation_angle, Vector3.right);
+            FR.transform.rotation = Quaternion.AngleAxis(rotation_angle, Vector3.right);
+            RL.transform.rotation = Quaternion.AngleAxis(rotation_angle, Vector3.right);
+            RR.transform.rotation = Quaternion.AngleAxis(rotation_angle, Vector3.right);
+
+
+        }
+    }
+
 }
---
+++ Assets/Scripts/PickerSceneController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PickerSceneController : MonoBehaviour {
+
+    public List<GameObject> cars;
+    public List<Material> cars_materials;
+
+    List<Color> avaliable_colors;
+    Vector3[] positions;
+
+    int currentPosition;
+    int currentColor;
+
+	// Use this for initialization
+	void Start () {
+        positions = new Vector3[4];
+        positions[0] = new Vector3(68.4f, -55.7f, 126);
+        positions[1] = new Vector3(76.5f, -55.7f, 126);
+        positions[2] = new Vector3(86.0f, -55.7f, 126);
+        positions[3] = new Vector3(93.9f, -55.7f, 126);
+        currentPosition = 0;
+        currentColor = 0;
+        for (int i = 0; i < cars.Count; i++) {
+            Object.DontDestroyOnLoad(cars[i]);
+        }
+
+        avaliable_colors = new List<Color>();
+
+        createColors(avaliable_colors);
+
+    }
+    public void createColors(List<Color> lst) {
+
+        lst.Add(Color.gray);
+        lst.Add(Color.black);
+        lst.Add(Color.blue);
+        lst.Add(Color.cyan);
+        lst.Add(Color.green);
+        lst.Add(Color.red);
+        lst.Add(Color.yellow);
+        lst.Add(Color.white);
+        lst.Add(new Color(0.2f, 0.2f, 0.2f, 1));
+        lst.Add(Color.grey);
+        lst.Add(new Color(0.7f, 0.7f, 0.7f, 1));
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown("d"))
+            currentPosition = getNextCircular(currentPosition, positions.Length);
+
+        if (Input.GetKeyDown("a"))
+            currentPosition = getPreviusCircular(currentPosition, positions.Length);
+
+        gameObject.transform.position = positions[currentPosition];
+
+        if (Input.GetKey("right")) 
+            cars[currentPosition].transform.Rotate(Vector3.up, 1);
+            
+        if (Input.GetKey("left")) 
+            cars[currentPosition].transform.Rotate(Vector3.up, -1);
+
+        if (Input.GetKeyDown("up")) {
+            currentColor = getNextCircular(currentColor, avaliable_colors.Count);
+            cars_materials[currentPosition].color = avaliable_colors[currentColor];
+        }
+
+        if (Input.GetKeyDown("down")) {
+            currentColor = getPreviusCircular(currentColor, avaliable_colors.Count);
+            cars_materials[currentPosition].color = avaliable_colors[currentColor];
+        }
+
+
+        Debug.Log(currentColor);
+
+        if (Input.GetKey("space")) {
+            SceneManager.LoadScene(1);
+        }
+    }
+    public int getNextCircular(int current, int elements) {
+
+        current++;
+        if (current == elements)
+            current = 0;
+
+        return current;
+    }
+
+    public int getPreviusCircular(int current, int elements) {
+
+        current--;
+        if (current == -1)
+            current = elements-1;
+
+        return current;
+    }
+}
